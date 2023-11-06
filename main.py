703ae92982707888e40a1f77eb360dde89bb12db
zaid123khalid <zaidbinkhalid246@gmail.com>
2023-11-06 21:55:52 +0500

    Endpoint to create rooms from mobile app

--- main.py
+++ main.py
@@ -9,28 +9,22 @@ rooms = {}
 
 @socket.on('join')
 def join(data):
-    if 'room' in session:
-        room_code = session['room']
-        rooms[room_code]["clients"].append(request.sid)
-    elif data['room_code'] in rooms:
+    if data['room_code'] in rooms:
         room_code = data['room_code']
         rooms[room_code]["members"] += 1
         rooms[room_code]["clients"].append(request.sid)
 
-@socket.on('disconnect')
-def disconnect():
-    room = session['room']
-    rooms[room]["members"] -= 1
-    rooms[room]["clients"].remove(request.sid)
+@socket.on('leave')
+def leave(data):
+    if data['room_code'] in rooms:
+        room_code = data['room_code']
+        rooms[room_code]["members"] -= 1
+        rooms[room_code]["clients"].remove(request.sid)
+        rooms.pop(room_code, None) if rooms[room_code]['members'] == 0 else None
 
 @socket.on('message')
 def handle_message(message):
-    if 'room' in session:
-        room_code = session['room']
-        rooms[room_code]["messages"].append(message)
-        clients = rooms[room_code]["clients"]
-        emit('recieved_msg', message, broadcast=True, to=clients, include_self=False)
-    elif message['room_code'] in rooms:
+    if message['room_code'] in rooms:
         room_code = message['room_code']
         message = {"username": message['username'], "msg": message['msg']}
         rooms[room_code]["messages"].append(message)
@@ -47,27 +41,26 @@ def index():
         if name is None or name == '' or room_code is None or room_code == '':
             session['error'] = "All above fields are required"
             return redirect(url_for('index'))
-      
+
         if room_code in rooms and join != False:
-            rooms[room_code]["members"] += 1
             session['username'] = name
             session['room'] = room_code
             return redirect(url_for('chat'))
-        
+
         if room_code not in rooms and create != False:
-            rooms[room_code] = {"members":1, "messages":[], "clients":[]}
+            rooms[room_code] = {"members":0, "messages":[], "clients":[]}
             session['username'] = name
             session['room'] = room_code
             return redirect(url_for('chat'))
-        
+
         if room_code not in rooms and join != False:
-            session['error'] = "Invalid room code"
+            session['error'] = "Room Not Found"
             return redirect(url_for('index'))
-        
+
         if room_code in rooms and create != False:
             session['error'] = "Room already exists"
             return redirect(url_for('index'))
-        
+
     error = session.pop('error', None)
     return render_template('index.html', error=error)
 
@@ -76,7 +69,7 @@ def chat():
     usernmae = session.get('username', None)
     room = session.get('room', None)
     messages = rooms.get(room, {}).get("messages", [])
-    return render_template('chat.html', username=usernmae, messages=messages)
+    return render_template('chat.html', username=usernmae, messages=messages, room=room)
 
 @app.route('/join_room', methods=['GET', 'POST'])
 def join_room():
@@ -89,7 +82,19 @@ def join_room():
         return {"error": "Invalid room code or username"}, 400
     if room not in rooms:
         return {"error": "Invalid room code"}, 400
-    
+    return {"error": "Something went wrong"}, 500
+
+@app.route('/create_room', methods=['GET', 'POST'])
+def create_room():
+    room = request.get_json()['room_code']
+    username = request.get_json()['username']
+    if room in rooms:
+        return {"error": "Room already exists"}, 400
+    if room is None or username is None:
+        return {"error": "Invalid room code or username"}, 400
+    if room not in rooms:
+        rooms[room] = {"members":0, "messages":[], "clients":[]}
+        return {"status": "success"}, 200
     return {"error": "Something went wrong"}, 500
 
 if __name__ == '__main__':
